3f9a1781e7316ca8a40e00457536c5ba351b1e6b
Юлия <juliarmolaeva@mail.ru>
2023-10-02 15:58:41 +0300

    дз 25 добавлена в сетевой чат аутентификация через базу данных

--- chat-server/src/main/java/org/example/Main.java
+++ chat-server/src/main/java/org/example/Main.java
@@ -1,8 +1,10 @@
 package org.example;
 
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        Server server = new Server(8080, new InMemoryAuthenticationProvider());
+    public static void main(String[] args) throws SQLException {
+        Server server = new Server(8080, new DatabaseAuthenticationProvider());
         server.start();
     }
 }
